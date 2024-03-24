b52a8f53224cfaa6fae6b9f5c65d3d082e7104db
eziraa <ezratgab@gmail.com>
2024-03-24 18:18:11 +0300

    adding views to to return products and a product detail

--- store/views.py
+++ store/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serialaizers import ProductSeriallizer
 
 
 @api_view()
 def ProductList(request):
-    return Response("Ok")
+    products = Product.objects.all()
+    serializer = ProductSeriallizer(products, many=True)
+
+    return Response(serializer.data)
+
+
+@api_view()
+def ProductDetail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSeriallizer(product)
+    return Response(serializer.data)
