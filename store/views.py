ba505ebdef480f4d4ea3422da302efe5ff09d867
eziraa <ezratgab@gmail.com>
2024-03-24 15:44:36 +0300

    initial commit

---
+++ store/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view()
+def ProductList(request):
+    return Response("Ok")
