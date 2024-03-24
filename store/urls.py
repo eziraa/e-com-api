7d6b449c70821dd8037423fca9f22a1de1452667
eziraa <ezratgab@gmail.com>
2024-03-24 21:40:55 +0300

    adding the collection routing  urls

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("products/", views.ProductList),
-    path("products/<int:id>/", views.ProductDetail)
+    path("products/<int:id>/", views.ProductDetail),
+    path("collections/", views.CollectionList),
+    path("collections/<int:id>/", views.CollectionDetail, name='collection-detail')
 ]
