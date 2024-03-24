f8e0fbff6f1c97de69d986d4ec59a559647244a8
eziraa <ezratgab@gmail.com>
2024-03-24 18:17:09 +0300

    Adding urls to route product and product list

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("products/", views.ProductList)
+    path("products/", views.ProductList),
+    path("products/<int:id>/", views.ProductDetail)
 ]
