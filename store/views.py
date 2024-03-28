ae6949f5c46b7db7dbd96d3f4e848b05b3669a63
eziraa <ezratgab@gmail.com>
2024-03-28 14:06:21 +0300

    Updating the ProductList and ProductDetail view to only retrieve data and not to update any data

--- store/views.py
+++ store/views.py
@@ -1,50 +1,26 @@
 from newsapi import NewsApiClient
-import requests
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework import status
-from .models import Product, Collection
+from .models import *
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-class ProductList(APIView):
-
+class ProductListView(APIView):
     def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(
-            products, many=True, context={'request': request})
-
+        collections = Collection.objects.all()
+        serializer = CollectionSerializer(collections, many=True)
         return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=201)
 
 
 class ProductDetail(APIView):
-
     def get(self, request, id):
         product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(
-            product, data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        product.delete()
-        return Response("Deleted", status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.data, status=status.HTTP_404_NOT_FOUND)
 
 
 class CollectionListView(APIView):
