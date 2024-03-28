e47848c462876a1fb94f3c57d910ac1e6fad3f74
eziraa <ezratgab@gmail.com>
2024-03-28 12:45:12 +0300

    Change the url according to the class based view

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("products/", views.ProductList),
-    path("products/<int:id>/", views.ProductDetail),
-    path("collections/", views.CollectionList),
-    path("collections/<int:id>/", views.CollectionDetail, name='collection-detail')
+    path("products/", views.ProductList.as_view(), name="get-products"),
+    path("products/<int:id>/", views.ProductDetail.as_view(), name="product-detail"),
+    path("collections/", views.CollectionListView.as_view(), name="get-collections"),
+    path("collections/<int:id>/", views.CollectionDetailView.as_view(),
+         name='collection-detail')
 ]
