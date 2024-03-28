a7834edefb0b55ccc1cc7fb880330e9dd6325e78
eziraa <ezratgab@gmail.com>
2024-03-28 13:20:40 +0300

    Creating Order and OrderItem serializer

--- store/serializers.py
+++ store/serializers.py
@@ -32,3 +32,15 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
