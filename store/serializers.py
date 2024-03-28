3e569d7ab2ef22f41fdfa6aa4a8aeba2a9ad7ee6
eziraa <ezratgab@gmail.com>
2024-03-28 13:19:49 +0300

    Creating customer  serializer and update the product serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,19 +2,6 @@ from rest_framework import serializers
 from .models import *
 
 
-class ProductSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Product
-        fields = ('id', 'title',
-                  'unit_price', 'collection', 'inventory', 'price_with_tax')
-    price_with_tax = serializers.SerializerMethodField(
-        method_name='calculate_tax',
-
-    )
-
-    def calculate_tax(self, product: Product):
-        return (product.unit_price.__float__() * 0.01).__round__(3)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -32,4 +19,16 @@ class CategorySerializer(serializers.ModelSerializer):
 class PromotionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promotion
+        fields = "__all__"
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
         fields = "__all__"
