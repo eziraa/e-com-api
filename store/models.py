e7c64caecdc30aa40e4af04b8c959f3611074fb8
eziraa <ezratgab@gmail.com>
2024-03-28 12:32:01 +0300

    Adding Payment model

--- store/models.py
+++ store/models.py
@@ -115,3 +115,9 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
 
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
