0fe8a30dad33151ffc0a15b8d01c2ca6a025e34e
eziraa <ezratgab@gmail.com>
2024-03-28 14:09:37 +0300

    adding promotion list and promotion detail view

--- store/views.py
+++ store/views.py
@@ -6,7 +6,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import *
-from .serializers import ProductSerializer, CollectionSerializer
+from .serializers import ProductSerializer, CollectionSerializer, PromotionSerializer
 
 
 class ProductListView(APIView):
@@ -34,4 +34,18 @@ class CollectionDetailView(APIView):
     def get(self, request, id):
         collection = get_object_or_404(Collection, pk=id)
         serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+
+
+class PromotionListView(APIView):
+    def get(self, request):
+        collections = Promotion.objects.all()
+        serializer = PromotionSerializer(promotions, many=True)
+        return Response(serializer.data)
+
+
+class PromotionDetailView(APIView):
+    def get(self, request, id):
+        promotion = get_object_or_404(Promotion, pk=id)
+        serializer = PromotionSerializer(promotion)
         return Response(serializer.data)
