ad1b14d8c82e7e638107dc970f24fa366905db34
eziraa <ezratgab@gmail.com>
2024-03-28 14:06:55 +0300

    update the urls according the views update

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views
+from . import models
 
 urlpatterns = [
-    path("products/", views.ProductList.as_view(), name="get-products"),
-    path("products/<int:id>/", views.ProductDetail.as_view(), name="product-detail"),
+    path("products/", views.ProductListView.as_view(),
+         name="get-products"),
+    path("products/<int:id>/", views.ProductDetail().__class__.as_view(),
+         name="product-detail"),
     path("collections/", views.CollectionListView.as_view(), name="get-collections"),
     path("collections/<int:id>/", views.CollectionDetailView.as_view(),
          name='collection-detail')
