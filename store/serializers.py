fa23d7b20bbef7a9f928c17617df98009a3547e1
eziraa <ezratgab@gmail.com>
2024-03-28 12:47:41 +0300

    cleaning code by removing unnecessary import

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Product, Collection, Source
+from .models import Product, Collection
 
 
 class ProductSerializer(serializers.ModelSerializer):
